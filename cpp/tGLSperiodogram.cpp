aea887598cea291b4d7ec7394ab969ab1b547ca1
Hilton Fernandes <fernandesh@yahoo.com>
2026-06-16 06:57:36 -0300

    2026-06-16 Added generation of pseudorandom numbers

--- cpp/tGLSperiodogram.cpp
+++ cpp/tGLSperiodogram.cpp
@@ -5,4 +5,30 @@
  * 
  * @author: @hgfernan
  */
+#include <cmath> // M_PI, cos(), sin()
+
+#include <vector>
+#include <random> // class random_device, class mt19937, class uniform_real_distribution
+#include <iostream>
+
+#include "GLSperiodogram.h" // GLS()
+
+int main(int argc, char* const* argv)
+{
+    // void GLS  (double* prange, int nfreq, double* x, double *y, double* w, int ndata, double* gls);
+
+    /// @remark set RNG seed for repeatable pseudorandom number generation
+    std::mt19937 gen((1 << 23) - 1);
+
+    std::uniform_real_distribution<> dis(-0.5, +0.5);
+
+    for (int n = 0; n < 10; ++n) {
+        std::cout << dis(gen) << ' ';
+    }
+
+    std::cout << '\n';
+
+    /// @remark Normal function termination
+    return 0;
+}
 
